c3678f169ce9933726e9dfcb91b328cce0d83833
JaeHong <dnflwoghddl@gmail.com>
2026-06-27 00:08:08 +0900

    feat: 가장 큰 수

---
+++ 프로그래머스/lv2/가장 큰 수.js
@@ -0,0 +1,5 @@
+function solution(numbers) {
+  const result = numbers.sort((prev, next)=> Number(String(next)+String(prev)) - Number(String(prev)+String(next)) ).join("");
+
+  return result[0] === '0' ? '0' : result;
+}
