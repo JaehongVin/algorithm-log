8554a7d65e3b53e702c698aa817a688ef0a2f6ba
JaeHong <dnflwoghddl@gmail.com>
2026-06-26 21:58:49 +0900

    feat: 행렬의 덧셈

---
+++ 프로그래머스/lv1/행렬의 덧셈.js
@@ -0,0 +1,11 @@
+function solution(arr1, arr2) {
+  const newArray = [];
+  
+  for (let i = 0; i < arr1.length; i++) {
+    newArray[i] = [];
+      for (let j = 0; j < arr1[i].length; j++) {
+          newArray[i][j] = arr1[i][j] + arr2[i][j];
+      }
+  }
+  return newArray;
+}
