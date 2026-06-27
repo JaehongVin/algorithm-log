1146b093842098a5d04be9b15231557d6e0b1ee4
JaeHong <dnflwoghddl@gmail.com>
2026-06-27 19:21:04 +0900

    feat: 올바른 괄호

---
+++ 프로그래머스/lv2/올바른 괄호.js
@@ -0,0 +1,19 @@
+function solution(s){
+  const stack = [];
+  const open = '(';
+  const close = ')';
+  
+  for (const scope of s) {
+      if (scope === open) {
+          stack.push(scope);
+          continue;
+      }
+      
+      if (stack.length < 1) return false;
+      
+      stack.pop();
+  }
+  
+  
+  return stack.length < 1;
+}
