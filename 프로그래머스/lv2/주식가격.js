a76dc79f5fc6ea914c8f4f6304581a613743e800
JaeHong <dnflwoghddl@gmail.com>
2026-06-28 16:20:40 +0900

    feat: 주식가격

---
+++ 프로그래머스/lv2/주식가격.js
@@ -0,0 +1,19 @@
+function solution(prices) {
+  const result = [];
+  const stack = [];    
+  
+  for (let i = 0; i < prices.length; i++) {
+      while(stack.length > 0 && prices[stack[stack.length-1]] > prices[i]) {
+          const top = stack.pop();
+          result[top] = i - top;
+      }
+      
+      stack.push(i);
+  }
+  
+  for (let i = 0; i < stack.length; i++) {
+      result[stack[i]] = prices.length - 1 - stack[i];
+  }
+
+  return result;
+}
